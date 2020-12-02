d1644d21457a7e87697a11f55fbd4e137ffbf63c
Vivan <vivanvatsa@gmail.com>
2020-12-02 19:13:38 +0530

    CMAKE build

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Vivan/Documents/GitHub/ds-salary-proj
+# Build directory: C:/Users/Vivan/Documents/GitHub/ds-salary-proj/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
