a1173e4769519b768925c9b61708da57270d98b5
Vivan <vivanvatsa@gmail.com>
2020-12-04 20:16:22 +0530

    edits

--- running.py
+++
@@ -1,7 +0,0 @@
-# %%
-import glassdoor_scraper as gs
-import pandas as pd
-
-path = "C:/Users/Vivan/Documents/GitHub/ds-salary-proj/geckodriver"
-
-df = gs.get_jobs("data scientist", 15, False, path, 15)
