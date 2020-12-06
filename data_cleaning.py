904cc0923ec479f58edaabb19438cf684d44500f
Vivan <vivanvatsa@gmail.com>
2020-12-06 19:33:47 +0530

    editing in salary estimate

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,6 +3,17 @@ import pandas as pd
 
 df = pd.read_csv("glassdoor_jobs.csv")
 
-df
+#%%
+# df
+
+# TO DO IN DATA CLEANING
+# -------------------
+# salary parsing
+# company name text only
+# state field
+# age of company
+# parsing of job description (PYTHON)
+#%%
+df = df[df["Salary Estimate"] != "-1"]
 
 # %%
