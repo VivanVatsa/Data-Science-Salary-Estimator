9db3aed0b078eddeff3d1e6205e9129328d2d445
Vivan <vivanvatsa@gmail.com>
2020-12-08 13:49:43 +0530

    data cleaning script

--- data_cleaning.py
+++ data_cleaning.py
@@ -37,17 +37,50 @@ df["company_txt"] = df["Company Name"].apply(lambda x: x.split("\n")[0])
 df
 
 # state field
-
-# age of company
-# parsing of job description (PYTHON)
+#%%
+df.Location.value_counts()
 
 #%%
+# 2 ways to delete undesired column from the data frame
+# 1.
+# del df["Headquarters"]
+# df = df.drop("Headquarters", 1)
+df = df.drop("Competitors", 1)
+df
+# age of company
+#%%
+
+df["age"] = df.Founded.apply(lambda x: x if x < 1 else 2020 - x)
+df
+
+# parsing of job description (PYTHON)
+#%%
+# will check all job descriptions keyword - analysis
+# python
+df["analysis"] = df["Job Description"].apply(lambda x: 1
+                                             if "analysis" in x.lower() else 0)
+df.analysis.value_counts()
+#%%
+df["Job Description"][0]
 # df["hourly"] = df["Salary Estimate"].apply(lambda x: 1
 #                                            if "per hour" in x.lower() else 0)
 # df
 
 # %%
+df
 # df["employer_provided"] = df["Salary Estimate"].apply(lambda x: 1
 #                                            if "employer provided" in x.lower() else 0)
 # df
 # min_hr = minus_Kd.apply(lambda x: x.lower().replace("per hour". '').replace('employer provided salary:', ''))
+
+# %%
+# *df cleaned*
+df_out = df
+df_out
+# %%
+df_out.to_csv("GL_sal_data_cleaned.csv", index=False)
+
+# %%
+pd.read_csv("GL_sal_data_cleaned.csv")
+
+# %%
