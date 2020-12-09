447e27925d41bfa3684702ebd0d1e54bb6788822
Vivan <vivanvatsa@gmail.com>
2020-12-09 18:43:31 +0530

    data exploratory data analysis

---
+++ data_EDA.py
@@ -0,0 +1,12 @@
+#%%
+import pandas as pd
+
+# matplotlib = this is for data visualisation
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("GL_sal_data_cleaned.csv")
+df.head()
+
+# %%
+df.columns
