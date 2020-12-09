7f4c52943e50f40cc40b36f684c6ac635545b4f1
Vivan <vivanvatsa@gmail.com>
2020-12-09 17:13:59 +0530

    jupyter notebook checkpoints

---
+++ .ipynb_checkpoints/run-checkpoint.py
@@ -0,0 +1,14 @@
+# %%
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "C:/Users/Vivan/Documents/GitHub/ds-salary-proj/drivers/geckodriver"
+
+df = gs.get_jobs("data scientist", 25, False, path, 15)
+# df
+#%%
+df.to_csv("glassdoor_jobs.csv", index=False)
+
+# %%
+df
+# %%
