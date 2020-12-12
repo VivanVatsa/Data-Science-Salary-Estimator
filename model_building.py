66a44a7fa2dc6e3b9bdf3d2dc0b801cb80cd101e
Vivan <vivanvatsa@gmail.com>
2020-12-12 22:08:14 +0530

    init model_building

--- model_building.py
+++ model_building.py
@@ -1,9 +1,18 @@
+#%%
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+df = pd.read_csv("data_EDA.csv")
 # choose the relevant columns
+
+#%%
+df.columns
+
+#%%
+# df_model = [['Rating', 'Size']]
 # get dummy data
+
 # train test split
 # multiple linear regression
 # lasso regression
