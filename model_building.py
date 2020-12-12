141090a4e94027e1e3b327f7cf70251a95d8c44c
Vivan <vivanvatsa@gmail.com>
2020-12-12 21:55:12 +0530

    edits & rerun

---
+++ model_building.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# choose the relevant columns
+# get dummy data
+# train test split
+# multiple linear regression
+# lasso regression
+# random forest
+# tune models GridSearchCV
+# test end samples
