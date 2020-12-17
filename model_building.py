5caca72418ad8be744b856788a9fd12ec17bda48
Vivan <vivanvatsa@gmail.com>
2020-12-17 23:04:28 +0530

    Scikit model building

--- model_building.py
+++ model_building.py
@@ -10,7 +10,7 @@ df = pd.read_csv("data_eda.csv")
 df.columns
 
 #%%
-ddf_model = df[[
+df_model = df[[
     "avg_salary",
     "Rating",
     "Size",
@@ -36,14 +36,52 @@ ddf_model = df[[
 #%%
 # get dummy data
 df_dum = pd.get_dummies(df_model)
+df_dum
+
 # train test split
+#%%
+from sklearn.model_selection import train_test_split
+
+X = df_dum.drop("avg_salary", axis=1)
+y = df_dum.avg_salary.values
+
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=42)
+
 # multiple linear regression
+#%%
+import statsmodels.api as sm
+
+X_sm = sm.add_constant(X)
+model = sm.OLS(y, X_sm)
+
+#%%
+model.fit().summary()
+
 # lasso regression
+#%%
+from sklearn.linear_model import LinearRegression
+
+lm = LinearRegression()
+
+lm.fit(X_train, y_train)
 # random forest
 # tune models GridSearchCV
 # test end samples
 
 # %%
-type(df["average-salary"])
+def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+    	def twoSum(A, B):
+		A = collections.Counter(A)
+		B = collections.Counter(B)
+		C = collections.defaultdict(int)
+		for a in A:
+			for b in B:
+				C[a+b] += A[a] * B[b]
+		return C
 
-# %%
+	AB = twoSum(A, B)
+	CD = twoSum(C, D)
+	return sum(AB[ab] * CD[-ab] for ab in AB)
