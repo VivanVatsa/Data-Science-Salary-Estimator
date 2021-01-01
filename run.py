8bbd4c9441ba38d3f66bb555e94e5e6e3faf676a
Vivan Vatsa <56357691+VivanVatsa@users.noreply.github.com>
2021-01-01 14:19:38 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import glassdoor_scraper as gs
 import pandas as pd
 
-path = "C:/Users/Vivan/Documents/GitHub/ds-salary-proj/drivers/geckodriver"
+path = "C:/Users/<UserName>/Documents/GitHub/ds-salary-proj/drivers/geckodriver"
 
 df = gs.get_jobs("data scientist", 25, False, path, 15)
 # df
